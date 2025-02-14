3e9feaebd0bdeced8e28fb46fadc9a612a903e6f
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-14 12:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const removeColor = () => {
+    let select = document.getElementById("colorSelect");
+    let selectedOption = select.options[select.selectedIndex];
+
+    if (selectedOption) {
+        selectedOption.remove(); // Re
+
